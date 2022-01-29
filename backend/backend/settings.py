f8f8c75bbd30fda4bbd1c7b580bf1c5109361f13
hasanprakash <hasanchadaram888@gmail.com>
2022-01-29 21:47:42 +0530

    static files commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,11 +133,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'rest_framework_example/static')
+   os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
